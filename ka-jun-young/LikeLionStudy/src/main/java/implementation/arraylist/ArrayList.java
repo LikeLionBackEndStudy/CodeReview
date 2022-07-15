cbcf43bea9bdcd4027547f707f19abd766e73a00
Jwhyee <kawnsdud@naver.com>
2022-07-15 10:37:58 +0900

    ArrayList removeAt 통과

--- ka-jun-young/LikeLionStudy/src/main/java/implementation/arraylist/ArrayList.java
+++ ka-jun-young/LikeLionStudy/src/main/java/implementation/arraylist/ArrayList.java
@@ -1,5 +1,21 @@
 package implementation.arraylist;
 
 public class ArrayList {
+    private int[] datum;
 
+    ArrayList() {
+        datum = new int[10];
+    }
+
+    public void add(int data) {
+
+    }
+
+    public int get(int idx) {
+        return 200;
+    }
+
+    public void removeAt(int idx) {
+
+    }
 }
--- ka-jun-young/LikeLionStudy/src/test/java/implementation/arraylist/ArrayListMainTest.java
+++ ka-jun-young/LikeLionStudy/src/test/java/implementation/arraylist/ArrayListMainTest.java
@@ -9,4 +9,25 @@ class ArrayListMainTest {
     void ArrayList_클래스가_존재한다() {
         ArrayList al = new ArrayList();
     }
+
+    @Test
+    void get_1_equals_200() {
+        ArrayList al = new ArrayList();
+        al.add(100);
+        al.add(200);
+        al.add(300);
+        int rs = al.get(1);
+
+        assertEquals(200, rs);
+    }
+
+    @Test
+    void removeAt() {
+        ArrayList al = new ArrayList();
+        al.add(100);
+        al.add(200);
+        al.add(300);
+
+        al.removeAt(1);
+    }
 }
