ae5f9d8d99dd3c0c9d009010604c5ea15c59c7fc
Jwhyee <kawnsdud@naver.com>
2022-07-15 10:41:40 +0900

    ArrayList removeAt_size_1_줄어든다 통과

--- ka-jun-young/LikeLionStudy/src/main/java/implementation/arraylist/ArrayList.java
+++ ka-jun-young/LikeLionStudy/src/main/java/implementation/arraylist/ArrayList.java
@@ -2,24 +2,30 @@ package implementation.arraylist;
 
 public class ArrayList {
     private int[] datum;
+    private int size;
 
     ArrayList() {
         datum = new int[10];
+        size = 0;
     }
 
     public void add(int data) {
-
+        datum[size] = data;
+        size++;
     }
 
     public int get(int idx) {
-        return 200;
+        return datum[idx];
     }
 
     public void removeAt(int idx) {
-
+        for (int i = idx + 1; i < size; i++) {
+            datum[i - 1] = datum[i];
+        }
+        size--;
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 }
