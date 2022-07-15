631dc15c978afbbe42af91de1443159f613e5767
Jwhyee <kawnsdud@naver.com>
2022-07-15 10:35:05 +0900

    ArrayList 구현시작

--- ka-jun-young/LikeLionStudy/src/main/java/implementation/arraylist/ArrayList.java
+++ ka-jun-young/LikeLionStudy/src/main/java/implementation/arraylist/ArrayList.java
@@ -2,22 +2,4 @@ package implementation.arraylist;
 
 public class ArrayList {
 
-    private int[] datum;
-    private int size;
-
-    public void add(int data) {
-
-    }
-
-    public int get(int idx) {
-        return 200;
-    }
-
-    public void removeAt(int idx) {
-
-    }
-
-    public int size() {
-        return 3;
-    }
 }
--- ka-jun-young/LikeLionStudy/src/test/java/implementation/arraylist/ArrayListMainTest.java
+++ ka-jun-young/LikeLionStudy/src/test/java/implementation/arraylist/ArrayListMainTest.java
@@ -9,39 +9,4 @@ class ArrayListMainTest {
     void ArrayList_클래스가_존재한다() {
         ArrayList al = new ArrayList();
     }
-
-    @Test
-    void get_1_equals_200() {
-        ArrayList al = new ArrayList();
-        al.add(100);
-        al.add(200);
-        al.add(300);
-        int rs = al.get(1);
-
-        assertEquals(200, rs);
-    }
-
-    @Test
-    void removeAt() {
-        ArrayList al = new ArrayList();
-        al.add(100);
-        al.add(200);
-        al.add(300);
-
-        al.removeAt(1);
-    }
-
-    @Test
-    void removeAt을_실행하면_size가_1_줄어든다() {
-        ArrayList al = new ArrayList();
-        al.add(100);
-        al.add(200);
-        al.add(300);
-
-        assertEquals(3, al.size());
-
-        al.removeAt(1);
-
-        assertEquals(2, al.size());
-    }
 }
